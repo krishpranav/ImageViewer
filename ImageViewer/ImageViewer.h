841fdc00eaec9fa66cf2f127d830cdac3c1df519
krishpranav <krisna.pranav@gmail.com>
2023-03-08 19:49:51 +0530

    Initial Commit

---
+++ ImageViewer/ImageViewer.h
@@ -0,0 +1,18 @@
+//
+//  ImageViewer.h
+//  ImageViewer
+//
+//  Created by eCOM-Elango.a on 08/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageViewer.
+FOUNDATION_EXPORT double ImageViewerVersionNumber;
+
+//! Project version string for ImageViewer.
+FOUNDATION_EXPORT const unsigned char ImageViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageViewer/PublicHeader.h>
+
+
